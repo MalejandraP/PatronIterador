2720b2905b66003bcadaf3f60d55ee8e37959b4c
MalejandraP <mariaa.pinzonv@uqvirtual.edu.co>
2025-11-05 20:52:44 -0500

    Delete IteradorPatron directory

--- IteradorPatron/src/main/java/co/edu/uniquindio/poo/iteradorpatron/model/Collection.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uniquindio.poo.iteradorpatron.model;
-
-public interface Collection {
-    public Iterator<Cancion> createIterator();
-}
-
--- IteradorPatron/src/main/java/co/edu/uniquindio/poo/iteradorpatron/model/Iterator.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.poo.iteradorpatron.model;
-
-public interface Iterator<T> {
-    public boolean hasNext();
-    public T next();
-
-    public boolean hasPrevious();
-    public T previous();
-}
